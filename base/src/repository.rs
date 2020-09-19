da231e6049c0679d1f816fe6b2da2fa69d53e5eb
Vivian Hellyer <vivian@hellyer.dev>
2020-09-19 14:16:49 -0400

    base/repository: don't rename traits
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- base/src/repository.rs
+++ base/src/repository.rs
@@ -1,4 +1,4 @@
-use super::{backend::Backend as BackendTrait, entity::Entity as EntityTrait};
+use super::{backend::Backend, entity::Entity};
 use futures_util::{
     future::{self, FutureExt, TryFutureExt},
     stream::Stream,
@@ -18,52 +18,52 @@ pub type RemoveEntitiesFuture<'a, E> = Pin<Box<dyn Future<Output = Result<(), E>
 pub type UpsertEntityFuture<'a, E> = Pin<Box<dyn Future<Output = Result<(), E>> + Send + 'a>>;
 pub type UpsertEntitiesFuture<'a, E> = Pin<Box<dyn Future<Output = Result<(), E>> + Send + 'a>>;
 
-pub trait Repository<Entity: EntityTrait, Backend: BackendTrait> {
+pub trait Repository<E: Entity, B: Backend> {
     /// Retrieve an immutable reference to the backend that the repository is
     /// tied to.
-    fn backend(&self) -> &Backend;
+    fn backend(&self) -> &B;
 
     /// Get an entity by its ID in the cache.
-    fn get(&self, entity_id: Entity::Id) -> GetEntityFuture<'_, Entity, Backend::Error>;
+    fn get(&self, entity_id: E::Id) -> GetEntityFuture<'_, E, B::Error>;
 
     /// Stream a list of records of the entity.
-    fn list(&self) -> ListEntitiesFuture<'_, Entity, Backend::Error>;
+    fn list(&self) -> ListEntitiesFuture<'_, E, B::Error>;
 
     /// Remove an entity by its ID from the cache.
-    fn remove(&self, entity_id: Entity::Id) -> RemoveEntityFuture<'_, Backend::Error>;
+    fn remove(&self, entity_id: E::Id) -> RemoveEntityFuture<'_, B::Error>;
 
     /// Bulk remove multiple entities from the cache.
     ///
-    /// **Backend implementations**: a default implementation is provided that
+    /// **B implementations**: a default implementation is provided that
     /// will concurrently await [`remove`] calls for all provided entity IDs.
     /// This may not be optimal for all implementations, so you may want to
     /// implement this manually.
     ///
     /// [`remove`]: #tymethod.remove
-    fn remove_bulk<T: Iterator<Item = Entity::Id>>(
+    fn remove_bulk<T: Iterator<Item = E::Id>>(
         &self,
         entity_ids: T,
-    ) -> RemoveEntitiesFuture<'_, Backend::Error> {
+    ) -> RemoveEntitiesFuture<'_, B::Error> {
         future::try_join_all(entity_ids.map(|id| self.remove(id)))
             .map_ok(|_| ())
             .boxed()
     }
 
     /// Upsert an entity into the cache.
-    fn upsert(&self, entity: Entity) -> UpsertEntityFuture<'_, Backend::Error>;
+    fn upsert(&self, entity: E) -> UpsertEntityFuture<'_, B::Error>;
 
     /// Bulk upsert multiple entities in the cache.
     ///
-    /// **Backend implementations**: a default implementation is provided that
+    /// **B implementations**: a default implementation is provided that
     /// will concurrently await [`upsert`] calls for all provided entity IDs.
     /// This may not be optimal for all implementations, so you may want to
     /// implement this manually.
     ///
     /// [`upsert`]: #tymethod.upsert
-    fn upsert_bulk<T: Iterator<Item = Entity> + Send>(
+    fn upsert_bulk<T: Iterator<Item = E> + Send>(
         &self,
         entities: T,
-    ) -> UpsertEntitiesFuture<'_, Backend::Error> {
+    ) -> UpsertEntitiesFuture<'_, B::Error> {
         Box::pin(future::try_join_all(entities.map(|entity| self.upsert(entity))).map_ok(|_| ()))
     }
 }
